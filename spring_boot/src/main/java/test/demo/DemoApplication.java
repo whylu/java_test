5382d19b5660ac66472d37e8684ac1423ca483bc
ming <minglung.weng@nogle.com>
2020-10-20 14:43:20 +0800

    Using my spring boot starter

--- spring_boot/src/main/java/test/demo/DemoApplication.java
+++ spring_boot/src/main/java/test/demo/DemoApplication.java
@@ -3,9 +3,15 @@ package test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+//    |- @org.springframework.context.annotation.Configuration
+//    |- @org.springframework.context.annotation.ComponentScan
+//    `- @org.springframework.boot.autoconfigure.EnableAutoConfiguration
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
+
+
+
--- spring_boot/src/main/java/test/demo/controller/HelloController.java
+++ spring_boot/src/main/java/test/demo/controller/HelloController.java
@@ -1,15 +1,30 @@
 package test.demo.controller;
 
 
+
+import ming.demo.MingHelloTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import test.demo.DemoApplication;
+
+
+@SpringBootApplication
+//    |- @org.springframework.context.annotation.Configuration
+//    |- @org.springframework.context.annotation.ComponentScan
+//    `- @org.springframework.boot.autoconfigure.EnableAutoConfiguration
 
 @RestController
 public class HelloController {
-
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
     @GetMapping("/hello")
     public String hello(@RequestParam("name") String name) {
         return "hello, " + name;
     }
+
 }
