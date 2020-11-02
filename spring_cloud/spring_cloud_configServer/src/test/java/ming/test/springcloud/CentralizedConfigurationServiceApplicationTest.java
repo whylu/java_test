ec1240c43628ae5b177fa361653dc141461d8b4a
ming <minglung.weng@nogle.com>
2020-11-02 09:26:08 +0800

    Use MockMvc

--- spring_cloud/spring_cloud_configServer/src/test/java/ming/test/springcloud/CentralizedConfigurationServiceApplicationTest.java
+++ spring_cloud/spring_cloud_configServer/src/test/java/ming/test/springcloud/CentralizedConfigurationServiceApplicationTest.java
@@ -4,29 +4,34 @@ package ming.test.springcloud;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Map;
+import org.springframework.test.web.servlet.*;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
                 classes = CentralizedConfigurationServiceApplication.class)
 public class CentralizedConfigurationServiceApplicationTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() throws Exception {
-        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:8888/a-bootiful-client/prod", String.class);
-        String body = entity.getBody();
+        MvcResult mvcResult = mvc.perform(get("/a-bootiful-client/prod"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
 
+        String body = mvcResult.getResponse().getContentAsString();;
         System.out.println(body);
         assertThat(body).contains("XXX_prod");
         assertThat(body).contains("YYY_prod");
