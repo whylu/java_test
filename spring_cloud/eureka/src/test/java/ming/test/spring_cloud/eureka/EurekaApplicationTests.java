2bb490728a7a2313676a54c05a93cc7b3073c14e
ming <minglung.weng@nogle.com>
2020-11-18 16:54:27 +0800

    Add eureka practice

---
+++ spring_cloud/eureka/src/test/java/ming/test/spring_cloud/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ming.test.spring_cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
